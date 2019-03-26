94f81c40b10a54f5937660227483d98de8ad3a29
Diego Campos <caquinodiego@crece.uss.edu.pe>
2019-03-25 19:12:25 -0500

    initial commit

--- plugin.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-    #include<stdlib.h>
-    static void plugon() __attribute__((constructor));
-    void plugon() {
-        system("/usr/local/bin/score f6074d16-9ae2-491a-8f76-1ca21fb78b6c");
-    }
